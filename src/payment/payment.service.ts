41be70764e5d89ebf72ad8676e853b7c82efaa0e
zakeri-dev <dev@mzakeri.ir>
2024-10-12 22:19:02 +0330

    .

---
+++ src/payment/payment.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePaymentDto } from './dto/create-payment.dto';
+import { UpdatePaymentDto } from './dto/update-payment.dto';
+
+const stripe = require('stripe')(
+  'sk_test_51Q8lBDQ2aujTRTPd9fybVZBFD8fsVptgLB5946WwBRELlPNsKaB2eBovm77gXLolAmL5XxMPHVGIYe13nnBin1Bj00w4ptiZ1I',
+);
+
+@Injectable()
+export class PaymentService {
+  async createPayment(createPaymentDto: any) {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            product_data: {
+              name: createPaymentDto.description,
+              description: createPaymentDto.description,
+              images: [createPaymentDto.logo],
+            },
+            unit_amount: Math.round(createPaymentDto.price * 100),
+            // Provide the exact Product ID (for example, prod_1234) of the product you want to sell
+            // price: createPaymentDto.product,
+            // quantity: 1,
+          },
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `https://gate.xcuts.co.uk/return.html?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://gate.xcuts.co.uk/return.html?session_id={CHECKOUT_SESSION_ID}`,
+    });
+    return session;
+  }
+
+  findAll() {
+    return `This action returns all payment`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} payment`;
+  }
+
+  update(id: number, updatePaymentDto: UpdatePaymentDto) {
+    return `This action updates a #${id} payment`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} payment`;
+  }
+}
