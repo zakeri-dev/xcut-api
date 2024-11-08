1f4657e2586e77f569a93ed06287cbab6927ecaf
zakeri-dev <dev@mzakeri.ir>
2024-11-08 15:29:58 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://test.xcuts.co.uk',
+      'https://xcuts.co.uk',
+      'https://www.google.com',
+    ],
+  });
   await app.listen(5000);
 }
 bootstrap();
