b3f4bcceaa76d462381f725b0ac1aec8723b5f32
Mahmoodreza Zakeri <m.zakeri@jaguars.dev>
2025-01-02 17:51:52 +0330

    .

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -54,7 +54,9 @@ export class PaymentService {
               product_data: {
                 name: 'cart_full_sheets',
                 description: createPaymentDto.userid,
-                images: ['https://i.ibb.co/fxpS6k7/Untitled-1.jpg'],
+                images: [
+                  'https://cms.xcuts.co.uk/assets/ece59885-603f-463b-8f56-2c9dfbaeccdb',
+                ],
               },
               unit_amount: Math.round(sumArray(allprice) * 100),
               // Provide the exact Product ID (for example, prod_1234) of the product you want to sell
@@ -66,10 +68,10 @@ export class PaymentService {
           },
         ],
         mode: 'payment',
-        success_url: `https://gate.xcuts.co.uk/verify?session_id=${createPaymentDto.userid}`,
-        cancel_url: `https://gate.xcuts.co.uk/verify?session_id=${createPaymentDto.userid}`,
+        success_url: `https://gate.xcuts.co.uk/payment/verify?session_id=${createPaymentDto.userid}`,
+        cancel_url: `https://xcuts.co.uk/shop-online`,
       });
-      console.log('+++++++++', session);
+      // console.log('+++++++++', session);
       return {
         payment: session.url,
       };
@@ -77,49 +79,6 @@ export class PaymentService {
       console.error(error);
     }
 
-    // const user = await pb
-    //   .collection('users')
-    //   .getFirstListItem(`id='${createPaymentDto.userid}'`, {
-    //     expand: 'cart_full_sheets',
-    //   });
-    // console.log(user);
-
-    // const cardItems = await user?.expand?.cart_full_sheets;
-
-    // const allprice = cardItems?.map((item: any) => item?.full_sheet_price);
-    // function sumArray(arr) {
-    //   return arr.reduce((a, b) => a + b, 0);
-    // }
-    // console.log('allprice', sumArray(allprice));
-    // await this.PaymentSend(sumArray(allprice), user);
-  }
-
-  async PaymentSend(price, user) {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price_data: {
-            currency: 'eur',
-            product_data: {
-              name: 'cart_full_sheets',
-              description: user.id,
-              images: ['https://i.ibb.co/fxpS6k7/Untitled-1.jpg'],
-            },
-            unit_amount: Math.round(price * 100),
-            // Provide the exact Product ID (for example, prod_1234) of the product you want to sell
-            // price: createPaymentDto.product,
-            // quantity: 1,
-          },
-          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url: `https://gate.xcuts.co.uk/verify?session_id=${user.id}`,
-      cancel_url: `https://gate.xcuts.co.uk/verify?session_id=${user.id}`,
-    });
-    // console.log(session.url);
-    return session.url;
   }
 
   async verifyPayment() {
